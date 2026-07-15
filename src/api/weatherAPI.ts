3602a519d0b39f246c54c9abd66f24abd445ed4f
ryan <kabirraiyan23@gmail.com>
2026-07-15 15:52:48 +0600

    params added

--- src/api/weatherAPI.ts
+++ src/api/weatherAPI.ts
@@ -1,46 +1,47 @@
 import type {
-    ApiError,
+  ApiError,
   WeatherRequest,
-  WeatherResponse
+  WeatherResponse,
 } from "../types/types";
-
 
 export async function getWeather(
   params: WeatherRequest,
   signal?: AbortSignal
 ): Promise<WeatherResponse> {
 
+    
+  const query = new URLSearchParams({
+    lat: params.lat.toString(),
+    lon: params.lon.toString(),
+    days: String(params.days ?? 7),
+    ai: String(params.ai ?? true),
+    units: params.units ?? "metric",
+    lang: params.lang ?? "en",
+  });
 
   const response = await fetch(
-    `${import.meta.env.WEATHER_API_URL}/weather`,
+    `${import.meta.env.VITE_WEATHER_API_URL}/weather?${query.toString()}`,
     {
       signal,
-      headers:{
-        Authorization:
-        `Bearer ${import.meta.env.WEATHER_API_KEY}`
-      }
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_WEATHER_API_KEY}`,
+      },
     }
   );
 
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => null);
 
- if (!response.ok) {
+    const error: ApiError = {
+      status: response.status,
+      message:
+        errorData?.message ??
+        `Weather API failed with status ${response.status}`,
+      code: errorData?.code,
+    };
 
-  const errorData = await response.json().catch(() => null);
-
-
-  const error: ApiError = {
-    status: response.status,
-    message:
-      errorData?.message ??
-      `Weather API failed with status ${response.status}`,
-    code:
-      errorData?.code,
-  };
-
-
-  throw error;
-}
+    throw error;
+  }
 
   return response.json();
-
 }
